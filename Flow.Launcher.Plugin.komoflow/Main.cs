0906cf4c3139b134fab6e14247ee15caacb61c9c
pro470 <reynoldsadjei@gmx.de>
2024-06-24 23:43:10 +0200

    first commit

---
+++ Flow.Launcher.Plugin.komoflow/Main.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+using Flow.Launcher.Plugin;
+
+namespace Flow.Launcher.Plugin.komoflow
+{
+    public class komoflow : IPlugin
+    {
+        private PluginInitContext _context;
+
+        public void Init(PluginInitContext context)
+        {
+            _context = context;
+            StartNamedPipeServer();
+        }
+
+        public List<Result> Query(Query query)
+        {
+            return new List<Result>();
+        }
+
+        private void StartNamedPipeServer()
+        {
+            Task.Run(() =>
+            {
+                using (var pipeServer = new NamedPipeServerStream("komorebi-pipe"))
+                {
+                    Console.WriteLine("Waiting for connection.");
+                    pipeServer.WaitForConnection();
+                    Console.WriteLine("Client connected.");
+
+                    using (var sr = new StreamReader(pipeServer))
+                    {
+                        string message;
+                        while ((message = sr.ReadLine()) != null)
+                        {
+                            Console.WriteLine("Received: " + message);
+                            // Process the message as needed
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+Flow.Launcher.Plugin.komoflow
+==================
+
+A plugin for the [Flow launcher](https://github.com/Flow-Launcher/Flow.Launcher).
+
+### Usage
+
+    kc <arguments># Flow.Launcher.Plugin.komoflow

ebedddbb8dabd26c56186585e3dd6c29e44c5f7f
pro470 <reynoldsadjei@gmx.de>
2024-06-26 18:11:51 +0200

    initial commit for python branch

--- release.ps1
+++
@@ -1,2 +0,0 @@
-dotnet publish Flow.Launcher.Plugin.komoflow -c Release -r win-x64 --no-self-contained
-Compress-Archive -LiteralPath Flow.Launcher.Plugin.komoflow/bin/Release/win-x64/publish -DestinationPath Flow.Launcher.Plugin.komoflow/bin/komoflow.zip -Force

d6fa4e8c426d4c3cbfe5cdcb3db07d1c018a5185
pro470 <reynoldsadjei@gmx.de>
2024-06-30 18:16:07 +0200

    added App_focus class

--- plugin/komoflow.py
+++ plugin/komoflow.py
@@ -10,4 +10,4 @@ plugin.pipe = create_named_pipe(plugin.pipename)
 
 plugin.add_method(Query(plugin.komorebic, plugin.pipe, plugin.pipename))
 plugin.add_method(Context_menu(plugin.komorebic, plugin.pipe, plugin.pipename))
-
+plugin.add_method(App_focus(plugin.komorebic, plugin.pipe, plugin.pipename))
--- plugin/methods.py
+++ plugin/methods.py
@@ -1,5 +1,6 @@
-from pyflowlauncher import Method, ResultResponse
+from pyflowlauncher import Method, ResultResponse, Result
 from plugin.komorebic_client import WKomorebic
+from plugin.utils import *
 
 
 class Query(Method):
@@ -10,10 +11,27 @@ class Query(Method):
         self.pipename = pipename
 
     def __call__(self, query: str) -> ResultResponse:
+        state_json = state(self.pipe)
+        if not state_json['is_paused']:
+            self.application_focus(state_json)
+
         return self.return_results()
 
-    def application_focus(self, hwnd: int, exe: str):
-        return
+    def application_focus(self, state):
+
+        application_list = []
+
+        for monitor in state['monitors']['elements']:
+            for workspace in monitor['workspaces']['elements']:
+                for container in workspace['containers']['elements']:
+                    for window in container['windows']['elements']:
+                        application_list.append([window['exe'], window['hwnd'], window['title']])
+                        r = Result(
+                            Title=window['title'],
+                            SubTitle=f'''Exe: {window['exe']}, HWND: {window['hwnd']}'''
+                        )
+
+                        self.add_result(r)
 
 
 class Context_menu(Method):
@@ -25,3 +43,14 @@ class Context_menu(Method):
 
     def __call__(self, data) -> ResultResponse:
         return self.return_results()
+
+
+class App_focus(Method):
+
+    def __init__(self, komorebic: WKomorebic, pipe, pipename):
+        self.komorebic = komorebic
+        self.pipe = pipe
+        self.pipename = pipename
+
+    def __call__(self, hwnd, exe):
+        self.komorebic.focus_exe()
